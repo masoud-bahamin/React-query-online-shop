9b7bf4859b99c8630cf0535779aa2d131fe19bf8
Masoud bahamin <bahaminwp@gmail.com>
2024-09-19 18:01:27 +0300

    create server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Use default middlewares (like CORS, static, etc.)
+server.use(middlewares);
+
+// Route the JSON Server to serve your mock database
+server.use(router);
+
+// Start the server
+const PORT = process.env.PORT || "https://react-query-online-shop.vercel.app/dashboard";
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
